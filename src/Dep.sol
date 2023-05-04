d8137c2f703311576d8cd7ef327bdc1329247d61
James Wenzel <jameswenzel@users.noreply.github.com>
2023-05-03 21:09:44 -0700

    fix override

--- src/Dep.sol
+++ src/Dep.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import {MyInterface} from "dep-core/interface/MyInterface.sol";
+import {MyContract} from "dep-core/MyContract.sol";
 import {MyStruct} from "dep-core/lib/MyStruct.sol";
 
-contract Dep is MyInterface {
+contract Dep is MyContract {
     function run(
         MyStruct calldata myStruct
-    ) external returns (MyStruct memory) {}
+    ) external override returns (MyStruct memory) {}
 }
