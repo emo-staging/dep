659096a528a4da924e0115af33d2308d39d0279f
James Wenzel <jameswenzel@users.noreply.github.com>
2023-05-03 21:07:16 -0700

    change imports for nt

--- src/Dep.sol
+++ src/Dep.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import {MyInterface} from "dep-core/interface/MyInterface.sol";
-import {MyStruct} from "dep-core/lib/MyStruct.sol";
+import {MyInterface} from "dep-types/MyInterface.sol";
+import {MyStruct} from "dep-types/lib/MyStruct.sol";
+import {MyContract} from "dep-core/MyContract.sol";
 
-contract Dep is MyInterface {
+contract Dep is MyContract {
     function run(
         MyStruct calldata myStruct
-    ) external returns (MyStruct memory) {}
+    ) external override returns (MyStruct memory) {}
 }
